367bfa62cd170378ed84a0b8dabd69e513a07b4d
Andreas Karlsen <apk@netcompany.com>
2015-10-01 09:02:33 +0200

    Initial commit

---
+++ OceanicAirlinesPackageSystem/BusinessLogic/Managers/RouteManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using BusinessLogic.ExternalInterfaces;
+
+namespace BusinessLogic.Managers
+{
+    public class RouteManager
+    {
+        private readonly IList<IExternalApi> _externalApis;
+
+        public RouteManager(IList<IExternalApi> externalApis)
+        {
+            _externalApis = externalApis;
+        }
+    }
+}
