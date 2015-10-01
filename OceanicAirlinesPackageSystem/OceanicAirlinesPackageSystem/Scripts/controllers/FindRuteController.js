838ea937d819ff5b9b781ce588c08e25eb58bd6a
nattfari <birgir.eliasson@gmail.com>
2015-10-01 18:08:52 +0200

    Api hæftet på oceanic module

--- OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Scripts/controllers/FindRuteController.js
+++ OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Scripts/controllers/FindRuteController.js
@@ -1,6 +1,5 @@
 ﻿angular.module('Oceanic').controller('FindRuteController', [
-    '$scope', function($scope, $filter) {
-
+    '$scope', 'Api', function($scope, Api) {
         $scope.rute = {fragtTyper:[]};
         $scope.byer = ['Tanger', 'Tunis', 'Tripoli', 'Cairo', 'De Kanariske Øer', 'Marakesh', 'Sahara', 'Omdurman', 'Suakin', 'Dakar', 'Timbuktu', 'Wadai', 'Darfur', 'Addis Abeba', 'Kap Guardafui', 'Sierra Leone', 'Guldkysten', 'Slavekysten', 'Bahrel Ghazal', 'Victoriasøen', 'Zanzibar', 'Congo', 'Kabalo', 'St. Helena', 'Luanda', 'Mocambique', 'Victoriafaldene', 'Amatave', 'Kap St. Marie', 'Dragebjerget', 'Hvalbugten', 'Kapstaden'];
 
@@ -8,6 +7,7 @@
         $scope.findRute = function() {
             console.log("Finder Rute");
             console.log($scope.rute);
+            console.log(Api.url);
         }
 
 
