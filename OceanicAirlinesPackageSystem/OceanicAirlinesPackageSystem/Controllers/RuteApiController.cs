be2691e20e1bb0871c37757d520e254da8273d31
unknown <mpf@T430SW7MPF.nclan.netcompany.dk>
2015-10-01 17:36:28 +0200

    tilføjet rute dto og ruteapicontroller

---
+++ OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Controllers/RuteApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessLogic.Data;
+using BusinessLogic.ExternalInterfaces;
+using BusinessLogic.Managers;
+using ExternalServices;
+using WebHost.DataContracts.DTOs;
+
+namespace WebHost.Controllers
+{
+    public class RuteApiController : ApiController
+    {
+        [Route("rute/soeg")]
+        [HttpPost]
+        public RuteResponseDTO GetRoutes([FromBody] RuteRequest ruteRequest)
+        {
+            IList<IExternalServicesApi> externalServices = new List<IExternalServicesApi>()
+            {
+                new MockService()
+            };
+
+            CalculationManager calculationManager = new CalculationManager();
+            //calculationManager.CalculateRouteTime(new @by(), new @by(), externalServices);
+
+            var result = new RuteResponseDTO();
+
+            return result;
+        }
+    }
+}
---
+++ OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/DataContracts/DTOs/RuteResponseDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHost.DataContracts.DTOs
+{
+    public class RuteRequest
+    {
+        public string FraBy { get; set; }
+        public string TilBy { get; set; }
+        public string Vaegt { get; set; }
+        public string[] FragtTyper { get; set; }
+        public int Dimension { get; set; }
+    }
+
+    public class RuteResponseDTO
+    {
+        public RuteRequestDTO RuteRequest { get; set; }
+        public List<RuteDTO> Ruter { get; set; }
+    }
+
+    public class RuteRequestDTO
+    {
+        public string FraBy { get; set; }
+        public string TilBy { get; set; }
+        public string Vaegt { get; set; }
+        public string[] FragtTyper { get; set; }
+        public int Dimension { get; set; }
+        public List<RuteDTO> Ruter { get; set; } 
+    }
+
+    public class RuteDTO
+    {
+        public string RuteType { get; set; }
+        public int TotalTid { get; set; }
+        public int TotalPris { get; set; }
+        public List<RuteTrinDTO> RuteTrin { get; set; } 
+    }
+
+    public class RuteTrinDTO
+    {
+        public string FraBy { get; set; }
+        public string TilBy { get; set; }
+        public int Tid { get; set; }
+        public int Pris { get; set; }
+        public string TransportType { get; set; }
+    }
+}
