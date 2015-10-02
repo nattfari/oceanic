1e4d7f7303c9a08a5b997fc4b830879c8304041b
unknown <mpf@T430SW7MPF.nclan.netcompany.dk>
2015-10-02 11:16:21 +0200

    ..

--- OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Scripts/controllers/RuteoversigtController.js
+++ OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Scripts/controllers/RuteoversigtController.js
@@ -1,8 +1,11 @@
 ﻿angular.module('Oceanic').controller('RuteoversigtController', [
     '$scope', 'userInteractionRepository', function ($scope, userInteractionRepository) {
+        $scope.loading = true;
         userInteractionRepository.hentSoegninger().then(function(result) {
             $scope.Ruter = result;
-        }, function(error) {
+            $scope.loading = false;
+        }, function (error) {
+            $scope.loading = false;
             $scope.errorMessage = "Kunne ikke hente ruter";
         });
     }
