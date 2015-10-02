c3aa73c4a1c05b928dae4b2ad069b81fe83b10a5
unknown <mpf@T430SW7MPF.nclan.netcompany.dk>
2015-10-02 10:39:25 +0200

    ffff

--- OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Controllers/BrugerAPIController.cs
+++ OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Controllers/BrugerAPIController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
 using BusinessLogic.Data;
 using WebHost.DataContracts.DTOs;
 
@@ -28,6 +29,51 @@ namespace WebHost.Controllers
             ctx.SaveChanges();
 
             return true;
+        }
+
+        [Route("user/hentAdministration")]
+        [HttpGet]
+        public AdministrationResponse getAdministrationInfo()
+        {
+            OADbContext ctx = new OADbContext();
+
+            var byer = ctx.by.ToList();
+
+            var pakkedimensioner = ctx.pakkeDimintioner;
+
+            var result = new AdministrationResponse()
+            {
+                PakkeDimensioner = new List<PakkeDimensionDto>(),
+                Lufthavne = new List<LufthavnDTO>()
+            };
+
+            foreach (var pakkedimension in pakkedimensioner)
+            {
+                var dimension = new PakkeDimensionDto();
+                var pakkerpriser = pakkedimension.pakkePris.Select(x => new PakkePriserDto()
+                {
+                    FromWeight = x.FromWeight,
+                    ToWeight = x.ToWeight,
+                    Price = x.Price,
+                    Id = x.Id
+                }).ToList();
+
+                dimension.PakkePriser = pakkerpriser;
+                dimension.DimensionNavn = pakkedimension.Name;
+
+                result.PakkeDimensioner.Add(dimension);
+            }
+
+            result.Lufthavne = Mapper.Map<List<@by>, List<LufthavnDTO>>(byer);
+
+            result.FragtTypeMultipliers = ctx.packetType.Select(x => new FragtTypeMultiplierDTO()
+            {
+                Multiplier = x.multiplier,
+                Name = x.PacketType1
+
+            }).ToList();
+
+            return result;
         }
 
         [Route("user/login")]
