50f20ea37807bdddb9eb1347d3e130e5db56e7f7
BrandonCKrueger <brandon.c.krueger@gmail.com>
2015-10-02 08:44:02 -0500

    Update userRepository.js
    Using `$q.defer()` is rarely needed, and is a very common promise anti-pattern. Try this as an alternative :smile:

--- OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Scripts/services/userRepository.js
+++ OceanicAirlinesPackageSystem/OceanicAirlinesPackageSystem/Scripts/services/userRepository.js
@@ -2,41 +2,33 @@
     '$http', '$q', 'Api', function($http, $q, Api) {
         return {
             createUser: function(user) {
-                var deferred = $q.defer();
-                $http({
+                return $http({
                     url: Api.url + '/user/create',
                     method: "POST",
                     data: user
-                }).success(deferred.resolve).error(deferred.reject);
-                return deferred.promise;
+                });
             },
             loginUser: function(user) {
-                var deferred = $q.defer();
-                $http({
+                return $http({
                     url: Api.url + '/user/login',
                     method: "POST",
                     data: user
-                }).success(deferred.resolve).error(deferred.reject);
-                return deferred.promise;
+                });
             },
             gemSoegning: function(id) {
-                var deferred = $q.defer();
-                $http({
+                return $http({
                     url: Api.url + '/internal/gemSoegning',
                     method: "POST",
                     data: {
                         forsendelseId : id
                     }
-                }).success(deferred.resolve).error(deferred.reject);
-                return deferred.promise;
+                });
             },
             hentSoegninger: function () {
-                var deferred = $q.defer();
-                $http({
+                return $http({
                     url: Api.url + 'rute/historik',
                     method: "GET"
-                }).success(deferred.resolve).error(deferred.reject);
-                return deferred.promise;
+                });
             }
         }
     }
